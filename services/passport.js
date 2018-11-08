b563f24aba7c687077c04b1f8299b601a6bff7fa
Dave Degeatano <davey.dee33@gmail.com>
2018-11-08 18:19:02 +0800

    adding the passport callbacks

--- services/passport.js
+++ services/passport.js
@@ -16,9 +16,12 @@ passport.use(
       User.findOne({ googleId: profile.id }).then(existingUser => {
         if (existingUser) {
           // We already have a record for that profile ID
+          done(null, existingUser); // Call the 'done' callback to tell Passport that we did what we wanted, and to continue with the authentication process.  Return 'null' errors, and the existingUser.
         } else {
           // No user record for this ID, we need to make a new record.
-          new User({ googleId: profile.id }).save();
+          new User({ googleId: profile.id })
+            .save()
+            .then(newUser => done(null, newUser));
         }
       });
     },

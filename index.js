9bf547198e83b84243fb2acd8c841e8e12985920
Dave Degeatano <davey.dee33@gmail.com>
2018-11-08 13:27:53 +0800

    Google OAuth authentication with passport.  Not complete yet but testing that we can successfully get the token and redirect to our callback URL on our server.

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback',
+    },
+    accessToken => {
+      console.log(accessToken);
+    },
+  ),
+);
+
+app.get(
+  '/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email'],
+  }),
+);
+
+app.get('/auth/google/callback', (req, res) => {
+  // TESTING that we can make it this far.
+  // Need to add the configurations in the Google developer console to allow the development and production domains
+  console.log('Redirect successful to callback URL');
+  res.json('Redirect successful to the callback URL!');
 });
 
-const port = process.env.PORT || 5000; // PORT value will be set when deployed on Heroku.
-app.listen(port);
+const PORT = process.env.PORT || 5000; // PORT value set when deployed on Heroku.
+app.listen(PORT);

0c5c8477a31e0ab4c970ef734db4bd2d03176552
Dave Degeatano <davey.dee33@gmail.com>
2018-11-08 17:58:05 +0800

    Now writing the googleId to MongoDB with a mongoose model and some magic.

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,9 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
 
 passport.use(
   new GoogleStrategy(
@@ -10,10 +13,7 @@ passport.use(
       callbackURL: '/auth/google/callback',
     },
     (accessToken, refreshToken, profile, done) => {
-      // Just log it for now to see what we get back from Google.
-      console.log('access token: ', accessToken);
-      console.log('refresh token: ', refreshToken);
-      console.log('profile: ', profile);
+      new User({ googleId: profile.id }).save();
     },
   ),
 );

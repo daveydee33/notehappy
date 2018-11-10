dccaa7e2be7dd7986ab234e2a95d0d097ed17071
Dave Degeatano <davey.dee33@gmail.com>
2018-11-10 20:24:38 +0800

    refactoring `passport.js` to use Async+Await instead of Promises.

--- services/passport.js
+++ services/passport.js
@@ -23,18 +23,19 @@ passport.use(
       callbackURL: '/auth/google/callback',
       proxy: true, // see: Fixing Heroku Proxy Issues
     },
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ googleId: profile.id }).then(existingUser => {
-        if (existingUser) {
-          // We already have a record for that profile ID
-          done(null, existingUser); // Call the 'done' callback to tell Passport that we did what we wanted, and to continue with the authentication process.  Return 'null' errors, and the existingUser.
-        } else {
-          // No user record for this ID, we need to make a new record.
-          new User({ googleId: profile.id })
-            .save()
-            .then(newUser => done(null, newUser));
-        }
-      });
+
+    async (accessToken, refreshToken, profile, done) => {
+      const existingUser = await User.findOne({ googleId: profile.id });
+
+      if (existingUser) {
+        // We already have a record for that profile ID
+        done(null, existingUser); // Call the 'done' callback to tell Passport that we did what we wanted, and to continue with the authentication process.  Return 'null' errors, and the existingUser.
+      } else {
+        // No user record for this ID, we need to make a new record.
+        const newUser = await new User({ googleId: profile.id }).save();
+
+        done(null, newUser);
+      }
     },
   ),
 );

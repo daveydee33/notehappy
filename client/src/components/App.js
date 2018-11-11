233e8bf0d0eabdd324a64ce2b3b48a82ce61a79b
Dave Degeatano <davey.dee33@gmail.com>
2018-11-11 14:05:39 +0800

    Initial setup of React Router, simple components with routing logic

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+const Header = () => <h2>Header</h2>;
+const Dashboard = () => <h2>Dashboard</h2>;
+const ItemNew = () => <h2>ItemNew</h2>;
+const Landing = () => <h2>Landing</h2>;
 
 const App = () => {
-  return <div>Hi There!</div>;
+  // return <div>Hi There!</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/items" component={Dashboard} />
+          <Route path="/items/new" component={ItemNew} />
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;

2673f66c0ccec029cdf1c2a78aa59e6bce1aa8e9
Dave Degeatano <davey.dee33@gmail.com>
2018-11-11 17:16:57 +0800

    Action creator for 'fetchUser' to check if user is logged in.  Installed redux, redux-thunk, axios.
    Will refactor next

---
+++ client/src/actions/index.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+import { FETCH_USER } from './types';
+
+const fetchUser = () => {
+  return function(dispatch) {
+    axios.get('/api/current_user').then(res =>
+      dispatch({
+        type: FETCH_USER,
+        payload: res,
+      }),
+    );
+  };
+};

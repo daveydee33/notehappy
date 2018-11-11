baccfa4c6eb3f2629cf38ae28769a44a6a9df6f1
Dave Degeatano <davey.dee33@gmail.com>
2018-11-11 16:46:22 +0800

    Header.js navbar starting point - a simple blue navbar, with Bootstrap classes

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,7 +2,51 @@ import React, { Component } from 'react';
 
 class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+          <a href="/" className="navbar-brand">
+            NoteHappy
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav mr-auto">
+              {/* Collections */}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  {' '}
+                  Profiles
+                </a>
+              </li>
+            </ul>
+
+            <ul className="navbar-nav ml-auto">
+              {/* Sign up */}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Register
+                </a>
+              </li>
+
+              {/* Login */}
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Login
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
   }
 }
 

92eab79313b5aaeb4958e7cae045303c82c13b5d
Dave Degeatano <davey.dee33@gmail.com>
2018-11-11 18:12:52 +0800

    wiring up the App component to call action creators.  Testing the `fetchUser` call.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions'; // ie,  ../actions/index.js.  Assign all of the actions to this 'actions' object.
+
 import Header from './Header';
 
 const Dashboard = () => <h2>Dashboard</h2>;
 const ItemNew = () => <h2>ItemNew</h2>;
 const Landing = () => <h2>Landing</h2>;
 
-const App = () => {
-  return (
-    <div className="container">
-      <BrowserRouter>
-        <div>
-          <Header />
-          <Route exact path="/" component={Landing} />
-          <Route exact path="/items" component={Dashboard} />
-          <Route path="/items/new" component={ItemNew} />
-        </div>
-      </BrowserRouter>
-    </div>
-  );
-};
+class App extends Component {
+  componentDidMount() {
+    this.props.fetchUser();
+  }
 
-export default App;
+  render() {
+    return (
+      <div className="container">
+        <BrowserRouter>
+          <div>
+            <Header />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/items" component={Dashboard} />
+            <Route path="/items/new" component={ItemNew} />
+          </div>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+export default connect(
+  null,
+  actions,
+)(App); // now all of the action creators (eg, fetchUser) are passed in as props to this `App` component.

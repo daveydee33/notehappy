8a3811b6ebe44904368e71f1441bafd636615566
Dave Degeatano <davey.dee33@gmail.com>
2018-11-12 20:38:03 +0800

    Installing body-parser module because we're going to need it later to handle POST/PUT/PATCH calls.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cookieSession = require('cookie-session');
 const passport = require('passport');
+const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
 require('./services/passport'); // make sure to use this file, but we're not exporting anything from it, so we don't need to assign to any variable.
@@ -13,6 +14,7 @@ mongoose.connect(
 
 const app = express();
 
+app.use(bodyParser.json()); // Needed for POST, PUT, PATCH requests.  Express by default won't parse the POST request payload. to be available as the req.body property.
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
@@ -22,7 +24,7 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app); // See: "Server Structure Refactor".  The "(app)" says to immediately invoke the previous with "app"
+require('./routes/authRoutes')(app); // See: "Server Structure Refactor".  The "(app)" says to immediately invoke the previous with "app".  The 'authRoutes' exports a function.  So this code here immediately calls that function with the express 'app' object.
 
 const PORT = process.env.PORT || 5000; // PORT value set when deployed on Heroku.
 app.listen(PORT);

029eba6b0fef7d3d340fce4b8a432779036f32ef
Dave Degeatano <davey.dee33@gmail.com>
2018-11-12 23:44:01 +0800

    Heroku production configuration settings.
    This is needed for things to work as a production build when deployed to Heroku.

--- index.js
+++ index.js
@@ -26,5 +26,18 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app); // See: "Server Structure Refactor".  The "(app)" says to immediately invoke the previous with "app".  The 'authRoutes' exports a function.  So this code here immediately calls that function with the express 'app' object.
 
+if (process.env.NODE_ENV === 'production') {
+  // Express will serve up production assets
+  // like our main.js file, or main.css file
+  app.use(express.static('client/build'));
+
+  // Express will serve up the index.html file
+  // if it doesn't recognize the route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000; // PORT value set when deployed on Heroku.
 app.listen(PORT);

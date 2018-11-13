f1db3c6e7021cf556ce5617b984a04c8730d4805
Dave Degeatano <davey.dee33@gmail.com>
2018-11-13 15:23:05 +0800

    Save new item POST request to MongoDB.

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,7 +4,7 @@ const requireLogin = require('../middlewares/requireLogin');
 const Item = mongoose.model('items');
 
 module.exports = app => {
-  app.post('/api/items/', requireLogin, (req, res) => {
+  app.post('/api/items/', requireLogin, async (req, res) => {
     const { title, body, tags } = req.body;
 
     const item = new Item({
@@ -15,7 +15,16 @@ module.exports = app => {
       dateCreated: Date.now(),
     });
 
-    // Do something with the received data (or error check it first).
-    // res.send(item);
+    // TODO: Validate data received first?
+
+    try {
+      const savedItem = await item.save();
+      res.send(savedItem);
+    } catch (err) {
+      res.status(422).send({
+        msg: 'Ouch! Something bad happened saving to the DB.',
+        error: err,
+      });
+    }
   });
 };

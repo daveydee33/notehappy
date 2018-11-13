d520f738d44ea5cd09263f3c4aeef8aba46a2a1e
Dave Degeatano <davey.dee33@gmail.com>
2018-11-13 13:57:25 +0800

    Temp code added for testing private routes (requireLogin) using axios in the browser (Chrome dev tool console).
    Can POST a new item now, but doesn't save to MongoDB yet.

--- client/src/index.js
+++ client/src/index.js
@@ -7,6 +7,11 @@ import reduxThunk from 'redux-thunk';
 import App from './components/App';
 import reducers from './reducers';
 
+// TEMP test code here so that we can use axios from our Chrome dev tools console to test our API routes that require login.  Testing from the browser console will include the cookies in the requests, which we can't easily do with postman.
+// Un-comment the following two lines temporarily for testing only.
+// import axios from 'axios';
+// window.axios = axios;
+
 const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
 
 ReactDOM.render(
--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,21 +4,18 @@ const requireLogin = require('../middlewares/requireLogin');
 const Item = mongoose.model('items');
 
 module.exports = app => {
-  app.post(
-    '/api/items/',
-    // requireLogin,
-    (req, res) => {
-      const { title, body, tags } = req.body;
+  app.post('/api/items/', requireLogin, (req, res) => {
+    const { title, body, tags } = req.body;
 
-      const item = new Item({
-        title,
-        body,
-        tags: tags.split(',').map(tag => tag.trim()),
-        // _user: req.user.id,
-        dateCreated: Date.now(),
-      });
+    const item = new Item({
+      title,
+      body,
+      tags: tags.split(',').map(tag => tag.trim()),
+      _user: req.user.id,
+      dateCreated: Date.now(),
+    });
 
-      res.send(item);
-    },
-  );
+    // Do something with the received data (or error check it first).
+    // res.send(item);
+  });
 };

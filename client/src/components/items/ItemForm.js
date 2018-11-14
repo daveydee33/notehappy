57ce719db6bbc6bdb024ca8b588ac8c1d4b1c755
Dave Degeatano <davey.dee33@gmail.com>
2018-11-14 22:01:11 +0800

    basic form styling only - no custom components

--- client/src/components/items/ItemForm.js
+++ client/src/components/items/ItemForm.js
@@ -1,19 +1,51 @@
 import React, { Component } from 'react';
 import { reduxForm, Field } from 'redux-form'; // the reduxForm helper to allow the redux form to communicate with the redux store.  Works similar to the “connect” helper function.
-import ItemFormField from './ItemFormField';
 
 class ItemForm extends Component {
   renderFormFields() {
     return (
-      <div>
-        <Field
-          label="Title"
-          type="text"
-          name="title"
-          component={ItemFormField}
-        />
-        <Field label="Body" type="text" name="body" component={ItemFormField} />
-        <Field label="Tags" type="text" name="tags" component={ItemFormField} />
+      <div className="form-group">
+        <div>
+          <label htmlFor="title">Title</label>
+
+          <Field
+            label="Title"
+            name="title"
+            component="input"
+            id="title"
+            placeholder="title"
+            type="text"
+            className="form-control form-control-lg"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="body">Body</label>
+
+          <Field
+            label="Body"
+            name="body"
+            component="textarea"
+            id="body"
+            placeholder="body"
+            type="text"
+            className="form-control form-control-lg"
+          />
+        </div>
+
+        <div>
+          <label htmlFor="tags">Tags</label>
+
+          <Field
+            label="Tags"
+            name="tags"
+            component="input"
+            id="tags"
+            placeholder="tags"
+            type="text"
+            className="form-control form-control-lg"
+          />
+        </div>
       </div>
     );
   }
@@ -24,7 +56,9 @@ class ItemForm extends Component {
         <h2>ItemForm</h2>
         <form onSubmit={this.props.handleSubmit(values => console.log(values))}>
           {this.renderFormFields()}
-          <button type="submit">Submit</button>
+          <button type="submit" className="btn btn-info">
+            Submit
+          </button>
         </form>
       </div>
     );

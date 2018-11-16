e4a8fa0d63b60b93467cab0fa3b1a1114d8a7c79
Dave Degeatano <davey.dee33@gmail.com>
2018-11-16 10:30:20 +0800

    Fixed an error when trying to split tags when tags field is empty

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -7,10 +7,14 @@ module.exports = app => {
   app.post('/api/items/', requireLogin, async (req, res) => {
     const { title, body, tags } = req.body;
 
+    if (tags) {
+      tags = tags.split(',').map(tag => tag.trim());
+    }
+
     const item = new Item({
       title,
       body,
-      tags: tags.split(',').map(tag => tag.trim()),
+      tags,
       _user: req.user.id,
       dateCreated: Date.now(),
     });

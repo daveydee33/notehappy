8df4556dd797c365b78339406a328af590747592
Dave Degeatano <davey.dee33@gmail.com>
2018-11-24 22:17:30 +0800

    fixed the bug with after adding new item.

--- client/src/reducers/itemReducer.js
+++ client/src/reducers/itemReducer.js
@@ -3,7 +3,7 @@ import { ADD_ITEM, FETCH_ITEMS } from '../actions/types';
 export default function(state = null, action) {
   switch (action.type) {
     case ADD_ITEM:
-      return action.payload;
+      return [...state, action.payload];
     case FETCH_ITEMS:
       return action.payload;
     default:

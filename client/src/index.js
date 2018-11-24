c7f83bd8beee9234a9c49e4c472eee68a4758055
Dave Degeatano <davey.dee33@gmail.com>
2018-11-24 20:50:32 +0800

    added Redux DevTools extension and configured.  For debugging in Dev.

--- client/src/index.js
+++ client/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension';
 
 import App from './components/App';
 import reducers from './reducers';
@@ -12,7 +13,11 @@ import reducers from './reducers';
 // import axios from 'axios';
 // window.axios = axios;
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+const store = createStore(
+  reducers,
+  {},
+  composeWithDevTools(applyMiddleware(reduxThunk)),
+);
 
 ReactDOM.render(
   <Provider store={store}>

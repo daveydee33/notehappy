c4b42b46abfb73ac6e44c2e439e208b68b9fb6cd
Dave Degeatano <davey.dee33@gmail.com>
2018-11-25 12:37:57 +0800

    - fixed proxy routing issue for APIs. - added GET and DELETE routes for single items

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -4,6 +4,36 @@ const requireLogin = require('../middlewares/requireLogin');
 const Item = mongoose.model('items');
 
 module.exports = app => {
+  // @route   GET api/item/:id
+  // @desc    Get single item by ID
+  // @access  Private
+  app.get('/api/item/:id', requireLogin, async (req, res) => {
+    const id = req.params.id;
+
+    const item = await Item.findOne({
+      _id: id,
+      _user: req.user.id,
+    });
+    // TODO: maybe create a condition to return something else if the item found, but user not the owner, etc?
+
+    res.send(item);
+  });
+
+  // @route   DELETE api/item/:id
+  // @desc    Delete single item by ID
+  // @access  Private
+  app.delete('/api/item/:id', requireLogin, async (req, res) => {
+    const id = req.params.id;
+
+    const item = await Item.findOneAndDelete({
+      _id: id,
+      _user: req.user.id,
+    });
+    // TODO: maybe create a condition to return something else if the item found, but user not the owner, etc?
+
+    res.send(item);
+  });
+
   app.get('/api/items', requireLogin, async (req, res) => {
     const items = await Item.find({
       _user: req.user.id,

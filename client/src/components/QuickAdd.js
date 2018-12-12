f8c55d4a3cdf64ef004c145be04799b5fa993288
Dave Degeatano <davey.dee33@gmail.com>
2018-12-12 14:25:24 +0800

    fixed formatting with classes. adds margin between form fields.

--- client/src/components/QuickAdd.js
+++ client/src/components/QuickAdd.js
@@ -15,31 +15,38 @@ class QuickAdd extends Component {
 
   render() {
     return (
-      <div className="quick-add">
+      <div className="quick-add ui segment">
         <h2>QuickAdd</h2>
         <form onSubmit={this.handleSubmit} className="ui form">
-          <input
-            type="text"
-            name="title"
-            id="title"
-            value={this.state.title}
-            onChange={e => this.setState({ title: e.target.value })}
-          />
-          <input
-            type="text"
-            name="tags"
-            id="tags"
-            value={this.state.tags}
-            onChange={e => this.setState({ tags: e.target.value })}
-          />
-          <textarea
-            name="body"
-            id="body"
-            cols="30"
-            rows="10"
-            value={this.state.body}
-            onChange={e => this.setState({ body: e.target.value })}
-          />
+          <div className="field">
+            <input
+              type="text"
+              name="title"
+              id="title"
+              value={this.state.title}
+              onChange={e => this.setState({ title: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            <input
+              type="text"
+              name="tags"
+              id="tags"
+              value={this.state.tags}
+              onChange={e => this.setState({ tags: e.target.value })}
+            />
+          </div>
+          <div className="field">
+            <textarea
+              name="body"
+              id="body"
+              cols="30"
+              rows="10"
+              value={this.state.body}
+              onChange={e => this.setState({ body: e.target.value })}
+            />
+          </div>
+
           <button type="submit" className="ui primary button">
             Add
           </button>
